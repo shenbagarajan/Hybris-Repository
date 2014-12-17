66b59fcaa946280e7e0934dabf01e5815307882d
shenbagarajan <shenbagarajan@raremile.com>
2014-12-16 18:39:38 -0800

    CuppyTrail
    Sample files to be checked in.

---
+++ cuppytrail/gensrc/com/cuppytrail/constants/GeneratedCuppytrailConstants.java
@@ -0,0 +1,55 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Dec 16, 2014 12:59:55 PM                    ---
+ * ----------------------------------------------------------------
+ */
+package com.cuppytrail.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedCuppytrailConstants
+{
+	public static final String EXTENSIONNAME = "cuppytrail";
+	public static class TC
+	{
+		public static final String STADIUM = "Stadium".intern();
+		public static final String STADIUMACCESS = "StadiumAccess".intern();
+		public static final String STADIUMTYPE = "StadiumType".intern();
+	}
+	public static class Attributes
+	{
+		public static class Match
+		{
+			public static final String STADIUM = "stadium".intern();
+		}
+	}
+	public static class Enumerations
+	{
+		public static class StadiumAccess
+		{
+			public static final String RAIL = "rail".intern();
+			public static final String ROAD = "road".intern();
+			public static final String PLANE = "plane".intern();
+		}
+		public static class StadiumType
+		{
+			public static final String ENCLOSED = "enclosed".intern();
+			public static final String OPENAIR = "openair".intern();
+		}
+	}
+	public static class Relations
+	{
+		public static final String STADIUMMATCHRELATION = "StadiumMatchRelation".intern();
+	}
+	
+	protected GeneratedCuppytrailConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
